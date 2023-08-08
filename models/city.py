2b4aa29e6bddf25df3a4c1b00d7bbf8c15fbea64
Anas Ribki <anasribki147@gmail.com>
2023-08-08 18:04:43 +0100

    Add task 9. More classes!

---
+++ models/city.py
@@ -0,0 +1,6 @@
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    state_id = ""
+    name = ""
---
+++ models/review.py
@@ -0,0 +1,7 @@
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    place_id = ""
+    user_id = ""
+    text = ""
