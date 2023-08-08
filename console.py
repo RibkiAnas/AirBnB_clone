bb0e52efe0ac46e24b53d19b25dc1082d493f2ff
Anas Ribki <anasribki147@gmail.com>
2023-08-08 17:45:03 +0100

    Add task 7. Console 0.1

--- console.py
+++ console.py
@@ -1,11 +1,103 @@
 #!/usr/bin/python3
 
 import cmd
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 
 class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb) "
+
+    def do_create(self, arg):
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in FileStorage.classes():
+            print("** class doesn't exist **")
+        else:
+            obj = eval(args[0])()
+            obj.save()
+            print(obj.id)
+
+    def do_show(self, arg):
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in FileStorage.classes():
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            key = args[0] + "." + args[1]
+            if key not in FileStorage.all():
+                print("** no instance found **")
+            else:
+                print(FileStorage.all()[key])
+                
+    def do_destroy(self, arg):
+        """Delete an instance based on the class name and id
+
+        Args:
+            arg: contains the class name and id to be deleted
+        """
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in FileStorage.classes():
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            key = args[0] + "." + args[1]
+            if key not in FileStorage.all():
+                print("** no instance found **")
+            else:
+                del FileStorage.all()[key]
+                FileStorage.save()
+                
+    def do_all(self, arg):
+        """Print all string representation of all instances based or not on the class name
+
+        Args:
+            arg: contains the class name to be printed
+        """
+        args = arg.split()
+        if len(args) == 0:
+            for value in FileStorage.all().values():
+                print(value)
+        elif args[0] not in FileStorage.classes():
+            print("** class doesn't exist **")
+        else:
+            for key, value in FileStorage.all().items():
+                if key.startswith(args[0]):
+                    print(value)
+                    
+    def do_update(self, arg):
+        """Update an instance based on the class name and id by adding or updating attribute
+
+        Args:
+            arg (): contains the class name and id to be updated
+        """
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in FileStorage.classes():
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            key = args[0] + "." + args[1]
+            if key not in FileStorage.all():
+                print("** no instance found **")
+            elif len(args) == 2:
+                print("** attribute name missing **")
+            elif len(args) == 3:
+                print("** value missing **")
+            else:
+                obj = FileStorage.all()[key]
+                setattr(obj, args[2], eval(args[3]))
+                obj.save()
 
     def do_quit(self, sta):
         """Quit command to exit the program"""
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,18 +7,24 @@ from models.base_model import BaseModel
 class FileStorage():
     __file_path = "file.json"
     __objects = {}
+    
+    @staticmethod
+    def classes():
+        return ['BaseModel', 'User', 'State', 'City', 'Amenity', 'Place', 'Review']
 
-    def all(self):
-        return self.__objects
+    @staticmethod
+    def all():
+        return FileStorage.__objects
 
     def new(self, obj):
         self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
 
-    def save(self):
+    @staticmethod
+    def save():
         dict_ = {}
-        for k, v in self.__objects.items():
+        for k, v in FileStorage.__objects.items():
             dict_[k] = v.to_dict()
-        with open(self.__file_path, mode="w+") as file:
+        with open(FileStorage.__file_path, mode="w+") as file:
             return file.write(json.dumps(dict_))
 
     def reload(self):
