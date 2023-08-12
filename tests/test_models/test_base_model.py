e80e511c8ffb161cdbd06e45b7d378c657840c66
younesse el-mouden <dayouness187@gmail.com>
2023-08-12 15:17:12 +0000

    Requirement

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,62 +3,26 @@
 """Test Base Model"""
 from datetime import datetime
 import unittest
-import inspect
-import models
 from models.base_model import BaseModel
 from unittest import mock
-from unittest.mock import MagicMock
 from time import sleep
-import pycodestyle
-
-
-class TestCodeFormat(unittest.TestCase):
-    """
-    A class to test pep8 on base_model file"""
-
-    def test_pycodestyle(self):
-        """
-        Test pep8 format
-        """
-        pycostyle = pycodestyle.StyleGuide(quiet=True)
-        result = pycostyle.check_files(['models/base_model.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-
-class Test_docstrings(unittest.TestCase):
-    """Test docstrings"""
-    @classmethod
-    def setup_class(self):
-        """
-        inspect.getmembers(object, [predicate])
-        Return all the members of an object in a list of (name, value)
-        pairs sorted by name
-        only members for which the predicate returns a true value are included
-        """
-        self.obj_members(BaseModel, inspect.isfunction)
-
-    def test_module_dostring(self):
-        """
-        Test for exist module docstrings
-        """
-        self.assertIsNotNone(models.base_model.__doc__,
-                             "base_model.py file needs a docstrings")
-        self.assertTrue(len(__doc__) > 0, " base_model.py have docstrings")
-        self.assertFalse(len(__doc__) < 0, " base_model  have docstrings")
 
 
 class Test_Class_BaseModel(unittest.TestCase):
     """Testing BaseModel class"""
+
     @mock.patch('models.storage')
     def test_BaseModel_instance(self, mock_storage):
+        """
+        Test BaseModel attributes
+        """
         instance = BaseModel()
         self.assertEqual(type(instance), BaseModel)
         self.assertTrue(type(instance) == BaseModel)
         self.assertIs(type(instance), BaseModel)
-        instance.name = "Michael"
-        instance.email = "haru@harumail.com"
-        instance.number = 300
+        instance.name = "Younesse"
+        instance.email = "ye@dyns.dev"
+        instance.number = 90
         expectec_attrs_types = {
             "id": str,
             "created_at": datetime,
@@ -77,19 +41,15 @@ class Test_Class_BaseModel(unittest.TestCase):
             "number",
             "__class__"]
         self.assertCountEqual(dict_inst.keys(), expectec_attrs)
-        self.assertEqual(dict_inst['name'], 'Michael')
-        self.assertEqual(dict_inst['email'], 'haru@harumail.com')
-        self.assertEqual(dict_inst['number'], 300)
+        self.assertEqual(dict_inst['name'], 'Younesse')
+        self.assertEqual(dict_inst['email'], 'ye@dyns.dev')
+        self.assertEqual(dict_inst['number'], 90)
         self.assertEqual(dict_inst['__class__'], 'BaseModel')
 
         for attr, types in expectec_attrs_types.items():
             with self.subTest(attr=attr, typ=types):
                 self.assertIn(attr, instance.__dict__)
                 self.assertIs(type(instance.__dict__[attr]), types)
-        self.assertTrue(mock_storage.new.called)
-        self.assertEqual(instance.name, "Michael")
-        self.assertEqual(instance.email, "haru@harumail.com")
-        self.assertEqual(instance.number, 300)
 
     def test_datetime(self):
         """
@@ -100,7 +60,7 @@ class Test_Class_BaseModel(unittest.TestCase):
         updated_at = datetime.now()
         self.assertEqual(created_at <= instance1.created_at, True)
         self.assertEqual(instance1.created_at <= updated_at, True)
-        sleep(2)
+        sleep(1)
         created_at = datetime.now()
         instance2 = BaseModel()
         updated_at = datetime.now()
@@ -116,12 +76,6 @@ class Test_Class_BaseModel(unittest.TestCase):
         instance1 = BaseModel()
         instance2 = BaseModel()
         instance3 = BaseModel()
-        list_instances = [instance1, instance2,
-                          instance3]
-        for instance in list_instances:
-            ins_uuid = instance.id
-            with self.subTest(uuid=ins_uuid):
-                self.assertIs(type(ins_uuid), str)
         self.assertNotEqual(instance1.id, instance2.id)
         self.assertNotEqual(instance1.id, instance3.id)
         self.assertNotEqual(instance2.id, instance3.id)
@@ -129,6 +83,7 @@ class Test_Class_BaseModel(unittest.TestCase):
     @mock.patch('models.storage')
     def test_save_method(self, mock_storage):
         """Testing save method"""
+
         instance = BaseModel()
         created_ats = instance.created_at
         sleep(2)
